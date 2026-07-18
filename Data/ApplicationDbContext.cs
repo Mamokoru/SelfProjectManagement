ec45a0f5cb35c3bfccb43980a5848ee477441835
Mamokoru <36095681+Mamokoru@users.noreply.github.com>
2026-07-18 16:25:32 +0530

    Initial ASP.NET Core API setup with EF Core and PostgreSQL

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskFlow.API.Entities;
+
+namespace TaskFlow.API.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+}
